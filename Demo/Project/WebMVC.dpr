79bd49fdaa72ce0c159bc24faa93e524a768624e
su <pearroom@yeah.net>
2020-06-09 14:36:55 +0800

    PRSoft

--- Demo/Project/WebMVC.dpr
+++ Demo/Project/WebMVC.dpr
@@ -5,7 +5,15 @@
 {       管理员权限启动delphi,管理员权限启动部署程序     }
 {                                                       }
 {*******************************************************}
-
+ {
+安装指南：https://my.oschina.net/delphimvc/blog/1581715
+我的博客：https://my.oschina.net/delphimvc
+相关视频：https://my.oschina.net/delphimvc/blog/4291418
+开发手册：http://129.211.87.47/doc/help.html
+讨论QQ群: 685072623
+开发工具:delphi xe10.3
+注意:win10系统以管理员权限运行
+}
 program WebMVC;
 {$APPTYPE GUI}
 //{$APPTYPE CONSOLE}
